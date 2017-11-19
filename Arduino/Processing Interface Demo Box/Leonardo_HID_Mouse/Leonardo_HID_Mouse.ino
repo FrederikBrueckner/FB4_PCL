88a47555fbf090da9d75bfa30edd5160e9dcb110
FrederikBrueckner <gitlab@prdct-dsgn.de>
2017-11-19 12:08:55 +0100

    Markup + documentation

--- Arduino/Processing Interface Demo Box/Leonardo_HID_Mouse/Leonardo_HID_Mouse.ino
+++ Arduino/Processing Interface Demo Box/Leonardo_HID_Mouse/Leonardo_HID_Mouse.ino
@@ -1,16 +1,15 @@
-#include <Rotary.h>
-#include <Mouse.h>
-#include <Arduino.h>
-#include <TM1637Display.h> //Einbinden der Bibliothekt zum ansteuern des Displays
+#include <Mouse.h>         // Arduino mouse emulation
+#include <Arduino.h>       // Arduino standard libraries
+#include <TM1637Display.h> // LED segment display library by avishorp https://github.com/avishorp
+#include <Rotary.h>        // rotary encoder library by Ben »Buxtronix« Buxton https://github.com/buxtronix
 
-#define CLK 10 //PIN 2 für den CLK PIN
-#define DIO 9 //PIN3 für den DIO 
+#define CLK 10 // TM1637 CLK PIN
+#define DIO 9  // TM1637 DIO PIN 
 
-//erzeugen des Display Objektes mit den Parametern für die PINs
+// Create display object
 TM1637Display display(CLK, DIO); 
 
-// Rotary encoder is wired with the common to ground and the two
-// outputs to pins 2 and 3.
+// Rotary encoder is wired with the common to ground and the two outputs to pins 2 and 3.
 Rotary rotary = Rotary(2, 3);
 
 /* Leonardo HID Joystick Mouse Encoder and Keyboard
@@ -28,19 +27,19 @@ Rotary rotary = Rotary(2, 3);
 // -----------------------------------------------------------------------------
 // constants
 
-const int               PinSW    = 7;         // Used for the rotary push button switch
-const int               sensitivity = 100;    // Higher sensitivity value = slower mouse, should be <= about 500
+const int               PinSW    = 7;         // used for the rotary push button switch
+const int               sensitivity = 100;    // higher sensitivity value = slower mouse, should be <= 500
 
 
 // -----------------------------------------------------------------------------
 // global vars
 
 volatile int            virtualPosition = 0;
-int                     horzPin = A0;          // Analog output of horizontal joystick pin
-int                     vertPin = A1;          // Analog output of vertical joystick pin
-int                     selPin = 8;            // Select button pin of joystick
-int                     vertZero, horzZero;    // Stores the initial value of each axis, usually around 512
-int                     vertValue, horzValue;  // Stores current analog output of each axis
+int                     horzPin = A0;          // analog output of horizontal joystick pin
+int                     vertPin = A1;          // analog output of vertical joystick pin
+int                     selPin = 8;            // select button pin of joystick
+int                     vertZero, horzZero;    // stores the initial value of each axis, usually around 512
+int                     vertValue, horzValue;  // stores current analog output of each axis
 int                     mouseClickFlag = 0;
 
 // -----------------------------------------------------------------------------
@@ -51,7 +50,7 @@ void loop();
 void setup();
 
 // -----------------------------------------------------------------------------
-// Interrupt service routine is executed when a HIGH to LOW transition is detected on CLK
+// interrupt service routine is executed when a HIGH to LOW transition is detected on CLK
 
 void rotate()  {
   unsigned char result = rotary.process();
@@ -61,7 +60,7 @@ void rotate()  {
       virtualPosition--;
       }
     
-    } // rotate interrupt routine
+    } // rotate
 
 // -----------------------------------------------------------------------------
 // setup
@@ -69,14 +68,14 @@ void rotate()  {
 void setup()
 { 
   Serial.begin(57600);
-  pinMode(horzPin, INPUT);  // joystick X
-  pinMode(vertPin, INPUT);  // joystick Y
-  pinMode(selPin, INPUT);   // joystick click
-  pinMode(PinSW, INPUT);    // rotary encoder click
-  digitalWrite(selPin, HIGH);  // Pull button select pin high
-  delay(1000);  // short delay to let outputs settle
+  pinMode(horzPin, INPUT);    // joystick X
+  pinMode(vertPin, INPUT);    // joystick Y
+  pinMode(selPin, INPUT);     // joystick click
+  pinMode(PinSW, INPUT);      // rotary encoder click
+  digitalWrite(selPin, HIGH); // pull button select pin high
+  delay(1000);                // short delay to let outputs settle
   vertZero = analogRead(vertPin);  // get the initial values
-  horzZero = analogRead(horzPin);  // Joystick should be in neutral position when reading these
+  horzZero = analogRead(horzPin);  // joystick should be in neutral position when reading these
 
   attachInterrupt(0, rotate, CHANGE);
   attachInterrupt(1, rotate, CHANGE);   // interrupt 1 == pin 2, interrupt 0 == pin 3 on Arduino Leonardo
@@ -84,11 +83,12 @@ void setup()
   Serial.println("Start");
 
   display.setBrightness(15);
-  uint8_t data[] = { 0xff, 0xff, 0xff, 0xff };
-  //Setzt die Anzahl der möglichen Segmente.
+  uint8_t data[] = { 0xff, 0xff, 0xff, 0xff }; // all segments on
+  
+  // set display segments.
   display.setSegments(data);
 
-  }
+  } // setup
 
 // -----------------------------------------------------------------------------
 // main loop
@@ -115,21 +115,21 @@ void loop()
     mouseClickFlag = 0;
     Mouse.release(MOUSE_LEFT);  // release the left button
   }
-  if (!(digitalRead(PinSW))) {        // check if pushbutton is pressed
+  if (!(digitalRead(PinSW))) {              // check if pushbutton is pressed
             virtualPosition = 0;            // if YES, then reset counter to ZERO
-            while (!digitalRead(PinSW)) {}  // wait til switch is released
+            while (!digitalRead(PinSW)) {}  // wait till switch is released
             delay(10);                      // debounce
-            Serial.println("Reset");        // Using the word RESET instead of COUNT here to find out a buggy encoder
+            Serial.println("Reset");        // using the word RESET instead of COUNT here to find out a buggy encoder
             }
-        if (virtualPosition != lastCount) {
+        if (virtualPosition != lastCount) { // show rotary counter on display
             lastCount = virtualPosition;
             Serial.print("Count = ");
             Serial.println(virtualPosition);
-            if (virtualPosition > -1){
+            if (virtualPosition > -1){      // positive numbers, include zero
               display.setSegments(0x40, 1, 4);
               display.showNumberDec(virtualPosition, true, 3, 1);
             }
-            else {
+            else {                          // negative numbers, show signed
               uint8_t clrs[] = {0x40};
               display.setSegments(clrs, 1, 4);
               display.showNumberDec(-virtualPosition, true, 3, 1);
@@ -137,4 +137,4 @@ void loop()
         }
             
   }
-}
+} // main loop
