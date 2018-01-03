8e90eb85ac644543195f7262d5a432f5644a9724
Frederik Brückner <gitlab@prdctdsgn.de>
2018-01-03 17:39:21 +0100

    Bugfix

--- Arduino/HY-SRF05/Multiple_Sensors/Multiple_Sensors.ino
+++ Arduino/HY-SRF05/Multiple_Sensors/Multiple_Sensors.ino
@@ -8,27 +8,29 @@
  * Send keystrokes depending on measured distance
  */
 
-const unsigned int sens_ping[] = {2, 4, 6, 8, 10}; // HY-SRF05 trigger pins
-const unsigned int sens_pong[] = {3, 5, 7, 9, 11}; // HY-SRF05 echo pins
+const unsigned int sens_ping[] = {2, 4, 6, 8, 14}; // HY-SRF05 trigger pins
+const unsigned int sens_pong[] = {3, 5, 7, 9, 15}; // HY-SRF05 echo pins
 const int near = 50; // Triggering distance 50cm 
 const int far = 150; // Triggering distance 150cm
 char lastZone = ' '; // Outer zone A, far zone B, near zone C 
 char activeZone ='A';// Zone to change to
+volatile int minDist = 999;
+const int numSense = 5; // Number of connected sensors
 
 long duration; // Time between trigger and echo
-volatile int distance, sens[5]; // Calculated distance in cm, last distance per sensor
+volatile int distance, sens[numSense]; // Calculated distance in cm, last distance per sensor
 
 void setup() {
   Serial.begin (9600);
   Keyboard.begin();
-  for (int i=0; i < 4; i++){ // Setup outputs and inputs (Arrays are zero indexed!)
+  for (int i=0; i < numSense; i++){ // Setup outputs and inputs (Arrays are zero indexed!)
     pinMode(sens_ping[i], OUTPUT);
     pinMode(sens_pong[i], INPUT);
   }
 } // End setup
  
 void loop() {
-  for (int i = 0; i < 4; i++){ // Take measurements for each sensor
+  for (int i = 0; i < numSense; i++){ // Take measurements for each sensor
     measure(sens_ping[i], sens_pong[i]);
     sens[i] = distance;
     Serial.print("Sensor ");
@@ -37,11 +39,12 @@ void loop() {
     Serial.print(sens[i]);
     Serial.println(" cm");
   }
-  if (minIndex() <= far) {
+
+  if (minDist <= far && minDist > near) {
     activeZone = 'B';
-    if (minIndex() <= near) {
+  }  
+  else if (minDist < near) {
       activeZone = 'C';
-      }
   }
   else {
       activeZone = 'A';
@@ -50,6 +53,9 @@ void loop() {
     Keyboard.print(activeZone);
     lastZone = activeZone;
   }
+  minDistance();
+  Serial.print(activeZone);
+  Serial.print(minDist);
 } // End loop
  
 void measure(int trigPin,int echoPin) { // Perform measurement and scale to centimeter
@@ -58,19 +64,24 @@ void measure(int trigPin,int echoPin) { // Perform measurement and scale to cent
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH, 15000L);  // Wait for echo, timeout after 15000 microseconds -> max. 257cm sens distance
-  distance = (duration/2) / 29.1; // Scale to cm
+  duration = pulseIn(echoPin, HIGH, 30000L);  // Wait for echo, timeout after 15000 microseconds -> max. 257cm sens distance
+  if (duration == 0){
+    distance = 999;
+  }
+  else {
+    distance = (duration/2) / 29.1; // Scale to cm
+  }
 } // End measure
 
 void minDistance() {
-  int minDist = 5000;
+  int minD = 5000;
   int minIndex = -1;
-  for (int i = 0; i< 4; i++){
-    if (sens[i] < minDist){
-      minDist = sens[i];
+  for (int i = 0; i< numSense; i++){
+    if (sens[i] < minD){
+      minD = sens[i];
       minIndex = i;
-      return minIndex;
     }
   }
+  minDist = minD;
 } // End minDistance
 
